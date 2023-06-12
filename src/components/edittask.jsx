65229e0844fbd0931c43bdab890021b77fc60995
123mufee <fathimamufeedakaruvally@gmail.com>
2023-06-12 09:36:25 +0530

    fix bug

--- src/components/edittask.jsx
+++ src/components/edittask.jsx
@@ -15,12 +15,13 @@ export default function Edittask({
   setIsLoading,
 }) {
   const navigate = useNavigate();
-  const [selectedUser, setSelectedUser] = useState("");
+  const [selectedUser, setSelectedUser] = useState(null);
   const [userList, setUserList] = useState([]);
-  const [taskDate, setTaskDate] = useState("");
-  const [taskTime, setTaskTime] = useState();
-  const [taskDescription, setTaskDescription] = useState("");
+  const [taskDate, setTaskDate] = useState(null);
+  const [taskTime, setTaskTime] = useState(null);
+  const [taskDescription, setTaskDescription] = useState(null);
   const [error, setError] = useState(null);
+  const [isLoading2, setisLoading2] = useState(true);
   const [userDetails, setUserDetails] = useState(null); // State to hold the selected user details
   const [tasks, setTasks] = useState([]); // State to hold the tasks
   const [showForm, setShowForm] = useState(false); // State to track the visibility of the form
@@ -37,13 +38,27 @@ export default function Edittask({
         }
       )
       .then((response) => {
-        console.log(response.data.results);
+        // console.log(response.data.results);
+
         setTaskDescription(response.data.results.task_msg);
         setTasks(response.data.results.task_time);
+        setTaskDate(response.data.results.task_date);
+        setSelectedUser(response.data.results.assigned_user);
       })
       .catch((e) => console.log(e));
     // console.log(edit)
   }, []);
+
+  useEffect(() => {
+    if (
+      taskDescription != null &&
+      taskDate != null &&
+      selectedUser != null &&
+      taskTime != null
+    ) {
+      setisLoading2(false);
+    }
+  }, [taskTime, taskDescription, taskDate, selectedUser]);
 
   useEffect(() => {
     const totalSeconds = Number(tasks);
@@ -115,7 +130,6 @@ export default function Edittask({
       task_msg: taskDescription,
     };
 
-    // console.log(taskTime);
     try {
       const response = await axios.put(
         ` https://stage.api.sloovi.com/task/lead_65b171d46f3945549e3baa997e3fc4c2/${taskvalue}?company_id=${companyId}`,
@@ -131,7 +145,6 @@ export default function Edittask({
 
       if (response.status === 200) {
         const data = response.data;
-
         setIsLoading(true);
         setediting("dashboard");
       } else {
@@ -142,100 +155,118 @@ export default function Edittask({
     }
   };
 
+  // if (isLoading2) {
+  //   return <div>Loading...</div>;
+  // }
+
+  // if (error) {
+  //   return <div>Error: {error}</div>;
+  // }
+
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="bg-white p-8 shadow-md rounded-lg">
-        <form onSubmit={handleTaskSubmit} className="mt-4">
-          <div className="mb-4">
-            <label htmlFor="user" className="block">
-              User:
-            </label>
-            <select
-              id="user"
-              value={selectedUser}
-              onChange={handleUserSelect}
-              className="border rounded p-1"
-              required
-            >
-              <option value="">Select User</option>
-              {userList.map((user) => (
-                <option key={user.id} value={user.id}>
-                  {user.name}
-                </option>
-              ))}
-            </select>
-          </div>
+    <>
+      {isLoading2 ? (
+        <div>Loading......</div>
+      ) : (
+        <div className="flex justify-center items-center h-screen">
+          <div className="bg-white p-8 shadow-md rounded-lg">
+            <form onSubmit={handleTaskSubmit} className="mt-4">
+              <div className="mb-4">
+                <label htmlFor="user" className="block">
+                  User:
+                </label>
+                <select
+                  id="user"
+                  value={selectedUser}
+                  onChange={handleUserSelect}
+                  className="border rounded p-1"
+                  required
+                >
+                  <option value="">Select User</option>
+                  {userList.map((user) => (
+                    <option key={user.id} value={user.id}>
+                      {user.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
 
-          {userDetails && (
-            <div className="mb-4">
-              <h3 className="text-lg font-bold">Selected User Details</h3>
-              <p>Name: {userDetails.name}</p>
-              <p>Email: {userDetails.email}</p>
-            </div>
-          )}
+              {userDetails && (
+                <div className="mb-4">
+                  <h3 className="text-lg font-bold">Selected User Details</h3>
+                  <p>Name: {userDetails.name}</p>
+                  <p>Email: {userDetails.email}</p>
+                </div>
+              )}
 
-          <div className="mb-4">
-            <label htmlFor="date" className="block text-xl">
-              Date:
-            </label>
-            <input
-              id="date"
-              type="date"
-              value={taskDate}
-              onChange={(e) => setTaskDate(e.target.value)}
-              className="border rounded p-2 text-xl"
-              required
-            />
-          </div>
+              <div className="mb-4">
+                <label htmlFor="date" className="block text-xl">
+                  Date:
+                </label>
+                <input
+                  id="date"
+                  type="date"
+                  value={taskDate}
+                  onChange={(e) => setTaskDate(e.target.value)}
+                  className="border rounded p-2 text-xl"
+                  required
+                />
+              </div>
 
-          <div className="mb-4">
-            <label htmlFor="time" className="block text-xl">
-              Time:
-            </label>
-            <input
-              id="time"
-              type="time"
-              value={taskTime}
-              onChange={(e) => setTaskTime(e.target.value)}
-              className="border rounded p-2 text-xl"
-              required
-            />
-          </div>
+              <div className="mb-4">
+                <label htmlFor="time" className="block text-xl">
+                  Time:
+                </label>
+                <input
+                  id="time"
+                  type="time"
+                  value={taskTime}
+                  onChange={(e) => setTaskTime(e.target.value)}
+                  className="border rounded p-2 text-xl"
+                  required
+                />
+              </div>
 
-          <div className="mb-4">
-            <label htmlFor="description" className="block text-xl" required>
-              Description:
-            </label>
-            <textarea
-              id="description"
-              value={taskDescription}
-              onChange={(e) => setTaskDescription(e.target.value)}
-              className="border rounded p-2 text-xl"
-              required
-            ></textarea>
+              <div className="mb-4">
+                <label htmlFor="description" className="block text-xl" required>
+                  Description:
+                </label>
+                <textarea
+                  id="description"
+                  value={taskDescription}
+                  onChange={(e) => setTaskDescription(e.target.value)}
+                  className="border rounded p-2 text-xl"
+                  required
+                ></textarea>
+              </div>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    gap: "10px",
+                  }}
+                >
+                  <button
+                    type="submit"
+                    className="bg-blue-500 text-white px-4 py-2 rounded"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    type="button"
+                    className="bg-red-700 text-white px-4 py-2 rounded"
+                    onClick={() => setediting("dashboard")}
+                  >
+                    Go back
+                  </button>
+                </div>
+              </div>
+            </form>
+            {/* )} */}
           </div>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <div
-              style={{ display: "flex", justifyContent: "center", gap: "10px" }}
-            >
-              <button
-                type="submit"
-                className="bg-blue-500 text-white px-4 py-2 rounded"
-              >
-                Edit
-              </button>
-              <button
-                type="button"
-                className="bg-red-700 text-white px-4 py-2 rounded"
-                onClick={() => setediting("dashboard")}
-              >
-                Go back
-              </button>
-            </div>
-          </div>
-        </form>
-        {/* )} */}
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 }
--- src/components/viewtask.jsx
+++ src/components/viewtask.jsx
@@ -10,8 +10,8 @@ import Edittask from "./edittask";
 const Viewtask = ({ accessToken, companyId, setActivePage }) => {
   const [tasks, setTasks] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [edittask, setedittask] = useState('')
-  const [editing, setediting] = useState(false)
+  const [edittask, setedittask] = useState("");
+  const [editing, setediting] = useState(false);
   // const tokenValue = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2ODY0NTIyNTksIm5iZiI6MTY4NjQ1MjI1OSwianRpIjoiNGVmNGMzZDktMmJkYS00NDg5LWIxZTgtYWM4NWI4NzBhN2Q2IiwiaWRlbnRpdHkiOnsibmFtZSI6IlNhcmF2YW5hbiBDIiwiZW1haWwiOiJzbWl0aHdpbGxzMTk4OUBnbWFpbC5jb20iLCJ1c2VyX2lkIjoidXNlcl84YzJmZjIxMjhlNzA0OTNmYTRjZWRkMmNhYjk3YzQ5MiIsImljb24iOiJodHRwOi8vd3d3LmdyYXZhdGFyLmNvbS9hdmF0YXIvY2Y5NGI3NGJkNDFiNDY2YmIxODViZDRkNjc0ZjAzMmI_ZGVmYXVsdD1odHRwcyUzQSUyRiUyRnMzLnNsb292aS5jb20lMkZhdmF0YXItZGVmYXVsdC1pY29uLnBuZyIsImJ5X2RlZmF1bHQiOiJvdXRyZWFjaCJ9LCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.-YW6d9QvX03DzyrbQGrVLd0uCXETRQ-HjykjpSkuZ5k"
 
   useEffect(() => {
@@ -21,9 +21,9 @@ const Viewtask = ({ accessToken, companyId, setActivePage }) => {
           `https://stage.api.sloovi.com/task/lead_65b171d46f3945549e3baa997e3fc4c2?company_id=${companyId}`,
           {
             headers: {
-              'Authorization':  `Bearer ${accessToken}`,
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
+              Authorization: `Bearer ${accessToken}`,
+              Accept: "application/json",
+              "Content-Type": "application/json",
             },
           }
         );
@@ -31,12 +31,11 @@ const Viewtask = ({ accessToken, companyId, setActivePage }) => {
         if (response.status === 200) {
           setTasks(response.data.results); // Assuming the response contains an array of tasks
         } else {
-          console.error('Failed to fetch tasks');
+          console.error("Failed to fetch tasks");
         }
       } catch (error) {
-        console.error('Error during task fetch:', error);
-      }
-      finally {
+        console.error("Error during task fetch:", error);
+      } finally {
         setIsLoading(false);
       }
     };
@@ -45,72 +44,83 @@ const Viewtask = ({ accessToken, companyId, setActivePage }) => {
   }, [isLoading]);
 
   const deleteTask = (taskId) => {
-    if (window.confirm('Are you sure you want to delete this task?')) {
+    if (window.confirm("Are you sure you want to delete this task?")) {
       handleDeleteTask(taskId);
       setTasks(tasks.filter((task) => task.id !== taskId));
-
     }
   };
 
   const handleDeleteTask = async (taskId) => {
-
     try {
       const response = await axios.delete(
         `https://stage.api.sloovi.com/task/lead_65b171d46f3945549e3baa997e3fc4c2/${taskId}?company_id=${companyId}`,
         {
           headers: {
-            'Authorization': `Bearer ${accessToken}`,
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+            Accept: "application/json",
+            "Content-Type": "application/json",
           },
         }
       );
 
       if (response.status === 200) {
-        toast.success('Task deleted successfully');
+        toast.success("Task deleted successfully");
         // console.log(response);
       } else {
-        toast.error('Failed to delete task');
+        toast.error("Failed to delete task");
       }
     } catch (error) {
-     
       console.log(error);
-      toast.error('Error during task deletion');
+      toast.error("Error during task deletion");
     }
-  }
+  };
   const editfunction = (taskId) => {
     // console.log(taskId);
-    setedittask(taskId)
-    setediting(true)
-  }
+    setedittask(taskId);
+    setediting(true);
+  };
 
   return (
     <>
-
-      {
-        editing == true ? (
-          <Edittask taskvalue={edittask} setediting={setediting} setIsLoading={setIsLoading} accessToken={localStorage.getItem("accesstoken")}  companyId={localStorage.getItem("companyid")}/>
-        ) : (
-          <div>
-            <h2>You have {tasks.length} Tasks</h2>
-            <ul>
-              {tasks.map((task) => (
-                <div key={task.id} className="bg-gray-200 text-gray-800 border border-gray-400 p-4 mb-4">
-                  <li>{task.task_msg}</li>
-                  <div className="flex justify-end mt-2">
-                    <button className="bg-blue-500 text-white px-4 py-2 rounded mr-2" onClick={() => editfunction(task.id)}>Edit</button>
-                    <button className="bg-red-500 text-white px-4 py-2 rounded" onClick={() => deleteTask(task.id)}>Delete</button>
-                  </div>
+      {editing == true ? (
+        <Edittask
+          taskvalue={edittask}
+          setediting={setediting}
+          accessToken={localStorage.getItem("accesstoken")}
+          companyId={localStorage.getItem("companyid")}
+          setIsLoading={setIsLoading}
+        />
+      ) : (
+        <div>
+          <h2>You have {tasks.length} Tasks</h2>
+          <ul>
+            {tasks.map((task) => (
+              <div
+                key={task.id}
+                className="bg-gray-200 text-gray-800 border border-gray-400 p-4 mb-4"
+              >
+                <li>{task.task_msg}</li>
+                <div className="flex justify-end mt-2">
+                  <button
+                    className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
+                    onClick={() => editfunction(task.id)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="bg-red-500 text-white px-4 py-2 rounded"
+                    onClick={() => deleteTask(task.id)}
+                  >
+                    Delete
+                  </button>
                 </div>
-              ))}
-            </ul>
-          </div>
-
-        )
-      }
+              </div>
+            ))}
+          </ul>
+        </div>
+      )}
     </>
-
   );
 };
 
-export default Viewtask
+export default Viewtask;
